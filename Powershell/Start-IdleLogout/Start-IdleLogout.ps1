3934aed6a624207f0fa45e63e269b876e0f1a7d3
HellBomb <33955773+HellBomb@users.noreply.github.com>
2020-06-30 19:38:48 -0400

    Create Start-IdleLogout.ps1

---
+++ Powershell/Start-IdleLogout/Start-IdleLogout.ps1
@@ -0,0 +1,39 @@
+<#	
+    .NOTES
+    VERSION     DATE			NAME						DESCRIPTION
+	___________________________________________________________________________________________________________
+	1.0     	30 June 2020	Nick W.						Initial version
+
+    Credits:
+        (1) Script Template: https://gist.github.com/9to5IT/9620683
+	
+#>
+
+[System.DateTime]$StartTime = [DateTime]::Now
+[System.DateTime]$CurrentTime = $StartTime
+
+IF (Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services") {
+	$Key = Get-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+	$KeyValue = $Key.GetValue("MaxDisconnectionTime", $Null)
+	IF ($KeyValue -ne $null) {
+		$IdleTime = New-TimeSpan -Seconds ($KeyValue/1000)
+	}
+}
+
+While ((New-TimeSpan -Start $StartTime -End ([DateTime]::now)) -lt $IdleTime) {
+	Start-Sleep -Seconds 30
+}
+
+$Process = [System.Diagnostics.Process]::new()
+$Process.StartInfo = [System.Diagnostics.ProcessStartInfo]::new()
+$Process.StartInfo.Arguments = ""
+$Process.StartInfo.FileName = "$($env:windir)\System32\logoff.exe"
+$Process.StartInfo.UseShellExecute = $False
+$Process.StartInfo.CreateNoWindow = $True
+$Process.StartInfo.RedirectStandardOutput = $True
+$Process.StartInfo.RedirectStandardError = $False
+$Process.StartInfo.RedirectStandardInput = $False
+$Process.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+$Process.StartInfo.LoadUserProfile = $False
+[Void]$Process.Start()
+[Void]$Process.WaitForExit()
