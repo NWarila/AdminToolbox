e0c195688a77f63f498c1ca9ad9540f156976f3f
HellBomb <33955773+HellBomb@users.noreply.github.com>
2020-07-01 12:58:18 -0400

    Update Start-IdleLogout.ps1

--- Powershell/Start-IdleLogout/Start-IdleLogout.ps1
+++ Powershell/Start-IdleLogout/Start-IdleLogout.ps1
@@ -1,3 +1,9 @@
+Param (
+    [Parameter(Mandatory=$True,Position=0)]
+    [ValidateSet('Start','Stop')]
+    [String]$Function,
+    [Int]$MaxStopAttempts = '5'
+)
 <#	
     .NOTES
     VERSION     DATE			NAME						DESCRIPTION
@@ -9,31 +15,48 @@
 	
 #>
 
-[System.DateTime]$StartTime = [DateTime]::Now
-[System.DateTime]$CurrentTime = $StartTime
+IF ($Function -eq 'Start') {
+    Try {
+        Get-Process -Name LogonUI -ErrorAction Stop
+    } Catch {
+        Write-Debug -Message "Current user logged on, ending script."
+        exit
+    }
 
-IF (Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services") {
-	$Key = Get-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
-	$KeyValue = $Key.GetValue("MaxDisconnectionTime", $Null)
-	IF ($KeyValue -ne $null) {
-		$IdleTime = New-TimeSpan -Seconds ($KeyValue/1000)
-	}
+    [System.DateTime]$StartTime = [DateTime]::Now
+    [System.DateTime]$CurrentTime = $StartTime
+
+    IF (Test-Path -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services") {
+	    $Key = Get-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"
+	    $KeyValue = $Key.GetValue("MaxDisconnectionTime", $Null)
+	    IF ($KeyValue -ne $null) {
+		    $IdleTime = New-TimeSpan -Seconds ($KeyValue/1000)
+	    }
+    }
+
+    While ((New-TimeSpan -Start $StartTime -End ([DateTime]::now)) -lt $IdleTime) {
+	    Start-Sleep -Seconds 30
+    }
+
+    $Process = [System.Diagnostics.Process]::new()
+    $Process.StartInfo = [System.Diagnostics.ProcessStartInfo]::new()
+    $Process.StartInfo.Arguments = ""
+    $Process.StartInfo.FileName = "$($env:windir)\System32\logoff.exe"
+    $Process.StartInfo.UseShellExecute = $False
+    $Process.StartInfo.CreateNoWindow = $True
+    $Process.StartInfo.RedirectStandardOutput = $True
+    $Process.StartInfo.RedirectStandardError = $False
+    $Process.StartInfo.RedirectStandardInput = $False
+    $Process.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
+    $Process.StartInfo.LoadUserProfile = $False
+    [Void]$Process.Start()
+    [Void]$Process.WaitForExit()
+} ElseIF ($Function -eq "Stop") {
+    [Int]$StopAttempts = '0'
+    $TaskState = Get-ScheduledTask -TaskName "Start-IdleLogoff" |Where-Object {$_.Principal.userID -match $env:USERNAME} |Select-Object -ExpandProperty State
+    While ($TaskState -eq 'Running' -AND $StopAttempts -le $MaxStopAttempts) {
+        Get-ScheduledTask -TaskName "Start-IdleLogoff" |Where-Object {$_.Principal.userID -match $env:USERNAME} |Stop-ScheduledTask
+        $TaskState = Get-ScheduledTask -TaskName "Start-IdleLogoff" |Where-Object {$_.Principal.userID -match $env:USERNAME} |Select-Object -ExpandProperty State
+        $StopAttempts++
+    }
 }
-
-While ((New-TimeSpan -Start $StartTime -End ([DateTime]::now)) -lt $IdleTime) {
-	Start-Sleep -Seconds 30
-}
-
-$Process = [System.Diagnostics.Process]::new()
-$Process.StartInfo = [System.Diagnostics.ProcessStartInfo]::new()
-$Process.StartInfo.Arguments = ""
-$Process.StartInfo.FileName = "$($env:windir)\System32\logoff.exe"
-$Process.StartInfo.UseShellExecute = $False
-$Process.StartInfo.CreateNoWindow = $True
-$Process.StartInfo.RedirectStandardOutput = $True
-$Process.StartInfo.RedirectStandardError = $False
-$Process.StartInfo.RedirectStandardInput = $False
-$Process.StartInfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
-$Process.StartInfo.LoadUserProfile = $False
-[Void]$Process.Start()
-[Void]$Process.WaitForExit()
