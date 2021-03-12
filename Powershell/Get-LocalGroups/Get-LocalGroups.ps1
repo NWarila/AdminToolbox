11c18ce5fe4bbef51382932719a589217e660d79
HellBomb <33955773+HellBomb@users.noreply.github.com>
2021-03-12 15:12:29 -0500

    Update Get-LocalGroups.ps1

--- Powershell/Get-LocalGroups/Get-LocalGroups.ps1
+++ Powershell/Get-LocalGroups/Get-LocalGroups.ps1
@@ -1,23 +1,22 @@
-New-Variable -name ADSI -value (([ADSI]"WinNT://localhost").psbase.children) -Force -ErrorAction Stop
-New-Variable -Name LocalGroups -Value (New-Object System.Collections.ArrayList) -Force -ErrorAction Stop
-New-Variable -Name GroupTypes -Force -Value @{"2" = "Global";"4" = "DomainLocal";"8" = "Universal"}
+New-Variable -name ADSI -value (([ADSI]"WinNT://$($ENV:ComputerName),computer").psbase.children) -Force -ErrorAction Stop
+New-Variable -Name LocalGroups   -Force -Value (New-Object System.Collections.ArrayList) -ErrorAction Stop
+New-Variable -Name GroupTypes    -Force -Value @{"2" = "Global";"4" = "DomainLocal";"8" = "Universal"}
 New-Variable -Name GroupLocation -Force -Value @{"$env:USERDOMAIN" = "Domain";"NT AUTHORITY" = "Local";"$ENV:COMPUTERNAME" = "Local"}
 $ADSI | Where-Object {$_.schemaClassName -match "group"} |select -Skip 1 | foreach-object {
-    Write-Host "Processing: $($_.Name.Value)"
-    $Group = [PSCustomObject]@{
+    New-Variable -Name CurrentGroup -Force -Value ([PSCustomObject]@{
         Name                       = $_.name.value -as [string]
         GroupType                  = $GroupTypes["$($_.GroupType.value)"] -As [String]
         Description                = $_.Description.value -as [string]
         SID                        = (New-Object System.Security.Principal.SecurityIdentifier($_.objectSid.value,0)).Value -as [String]            
         Members                    = New-Object System.Collections.ArrayList
-    }
+    })
     ([ADSI]$_.psbase.Path).psbase.Invoke("Members") | ForEach-Object {
-        [void]$Group.Members.Add([PSCustomObject]@{
+        [void]$CurrentGroup.Members.Add([PSCustomObject]@{
             Name      = $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null)
             SID       = (New-Object System.Security.Principal.SecurityIdentifier($_.GetType().InvokeMember("ObjectSID", 'GetProperty', $null, $_, $null),0)).value
             ADsPath   = $_.GetType().InvokeMember("ADsPath", 'GetProperty', $null, $_, $null)
             GroupType = $GroupLocation["$($_.GetType().InvokeMember('ADsPath', 'GetProperty', $null, $_, $null).split('/')[2])"]
         })
     }
-    [Void]$LocalGroups.Add($Group)
+    [Void]$LocalGroups.Add($CurrentGroup)
 }
